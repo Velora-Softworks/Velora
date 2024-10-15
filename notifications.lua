cba12e44cf8a56fc1463bef13196f708d0cde5f9
Ollie0000 <132388201+Ollie0000@users.noreply.github.com>
2024-10-15 17:52:48 +0200

    Create notifications.lua

---
+++ notifications.lua
@@ -0,0 +1,13 @@
+local Velora = game:GetService("CoreGui"):FindFirstChild("Velora")
+
+if not Velora then
+    Velora = Instance.new("ScreenGui")
+    Velora.Parent = game:GetService"CoreGui"
+    Velora.Name = 'Velora'
+    Velora.ResetOnSpawn = false
+    Velora.IgnoreGuiInset = true
+
+    game:GetObjects("rbxassetid://129923956313208").Parent = Velora
+end
+
+return require(Velora.Notifications:WaitForChild"Notifier")
