b270135180daec59656342ced3f67fd7ca40eb80
Ollie0000 <132388201+Ollie0000@users.noreply.github.com>
2024-10-21 17:19:11 +0200

    Update notifications.lua

--- notifications.lua
+++ notifications.lua
@@ -1,47 +1,191 @@
---[[
-
- @ Velora Notifications System
-
- - Buttons template:
- {
-	 {
-	 	Text = "Button";
-	 	Callback = funcion()
-	 		print("Button clicked!")
-	 	end;
-	 };
-	 {
-	 	Text = "Button 2";
-	 	Callback = funcion()
-	 		print("Button 2 clicked!")
-	 	end;
-	 }
- }
-
-]]
-
 -- Load
-local Velora = game:GetService("CoreGui"):FindFirstChild("Velora")
+local Velora = game:GetService('CoreGui'):FindFirstChild('Velora')
+local VeloraFunctions = loadstring(game:HttpGet('https://raw.githubusercontent.com/Velora-Softworks/Velora/refs/heads/main/functions.lua'))()
+local JustCreated
 
 if not Velora then
-    Velora = Instance.new("ScreenGui")
-    Velora.Parent = game:GetService"CoreGui"
-    Velora.Name = 'Velora'
-    Velora.ResetOnSpawn = false
-    Velora.IgnoreGuiInset = true
+	Velora = Instance.new('ScreenGui')
+	Velora.Parent = game:GetService'CoreGui'
+	Velora.Name = 'Velora'
+	Velora.ResetOnSpawn = false
+	Velora.IgnoreGuiInset = true
+	
+	JustCreated = true
 end
 
-if not Velora:FindFirstChild("Notifications") then
-    game:GetObjects("rbxassetid://129923956313208")[1].Parent = Velora
+if not Velora:FindFirstChild('Notifications') then
+	local G2L = {};
+	G2L["2"] = Instance.new("Frame", Velora);
+	G2L["2"]["BorderSizePixel"] = 0;
+	G2L["2"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+	G2L["2"]["Size"] = UDim2.new(0, 350, 0, 763);
+	G2L["2"]["Position"] = UDim2.new(0.791, 0, 0.133, 0);
+	G2L["2"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+	G2L["2"]["Name"] = [[Notifications]];
+	G2L["2"]["BackgroundTransparency"] = 1;
+
+
+	-- StarterGui.ScreenGui.Notifications.UIListLayout
+	G2L["3"] = Instance.new("UIListLayout", G2L["2"]);
+	G2L["3"]["HorizontalAlignment"] = Enum.HorizontalAlignment.Center;
+	G2L["3"]["Padding"] = UDim.new(0, 6);
+	G2L["3"]["VerticalAlignment"] = Enum.VerticalAlignment.Bottom;
+	G2L["3"]["SortOrder"] = Enum.SortOrder.LayoutOrder;
+
+
+	-- StarterGui.ScreenGui.Notifications.Template
+	G2L["4"] = Instance.new("Frame", G2L["2"]);
+	G2L["4"]["BorderSizePixel"] = 0;
+	G2L["4"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+	G2L["4"]["Size"] = UDim2.new(0, 350, 0, 141);
+	G2L["4"]["Position"] = UDim2.new(-0.08858, 0, 0.71171, 0);
+	G2L["4"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+	G2L["4"]["Name"] = [[Template]];
+
+
+	-- StarterGui.ScreenGui.Notifications.Template.UIGradient
+	G2L["5"] = Instance.new("UIGradient", G2L["4"]);
+	G2L["5"]["Rotation"] = 60;
+	G2L["5"]["Offset"] = Vector2.new(0.5, 0.5);
+	G2L["5"]["Color"] = ColorSequence.new{ColorSequenceKeypoint.new(0.000, Color3.fromRGB(26, 26, 26)),ColorSequenceKeypoint.new(1.000, Color3.fromRGB(61, 64, 70))};
+
+
+	-- StarterGui.ScreenGui.Notifications.Template.Title
+	G2L["6"] = Instance.new("TextLabel", G2L["4"]);
+	G2L["6"]["TextWrapped"] = true;
+	G2L["6"]["BorderSizePixel"] = 0;
+	G2L["6"]["TextXAlignment"] = Enum.TextXAlignment.Left;
+	G2L["6"]["TextScaled"] = true;
+	G2L["6"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+	G2L["6"]["TextSize"] = 14;
+	G2L["6"]["FontFace"] = Font.new([[rbxassetid://12187365364]], Enum.FontWeight.Bold, Enum.FontStyle.Normal);
+	G2L["6"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
+	G2L["6"]["BackgroundTransparency"] = 1;
+	G2L["6"]["Size"] = UDim2.new(0, 175, 0, 20);
+	G2L["6"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+	G2L["6"]["Text"] = [[Lorem ipsum]];
+	G2L["6"]["Name"] = [[Title]];
+	G2L["6"]["Position"] = UDim2.new(0, 50, 0, 17);
+
+
+	-- StarterGui.ScreenGui.Notifications.Template.Text
+	G2L["7"] = Instance.new("TextLabel", G2L["4"]);
+	G2L["7"]["TextWrapped"] = true;
+	G2L["7"]["BorderSizePixel"] = 0;
+	G2L["7"]["TextXAlignment"] = Enum.TextXAlignment.Left;
+	G2L["7"]["TextScaled"] = true;
+	G2L["7"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+	G2L["7"]["TextSize"] = 14;
+	G2L["7"]["FontFace"] = Font.new([[rbxassetid://12187365364]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+	G2L["7"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
+	G2L["7"]["BackgroundTransparency"] = 1;
+	G2L["7"]["Size"] = UDim2.new(0, 300, 0, 14);
+	G2L["7"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+	G2L["7"]["Text"] = [[Lorem ipsum dolor]];
+	G2L["7"]["Name"] = [[Text]];
+	G2L["7"]["Position"] = UDim2.new(0, 20, 0, 44);
+
+
+	-- StarterGui.ScreenGui.Notifications.Template.UICorner
+	G2L["8"] = Instance.new("UICorner", G2L["4"]);
+	G2L["8"]["CornerRadius"] = UDim.new(0.05, 0);
+
+
+	-- StarterGui.ScreenGui.Notifications.Template.Transparency
+	G2L["9"] = Instance.new("UIGradient", G2L["4"]);
+	G2L["9"]["Enabled"] = false;
+	G2L["9"]["Transparency"] = NumberSequence.new{NumberSequenceKeypoint.new(0.000, 0),NumberSequenceKeypoint.new(1.000, 1)};
+	G2L["9"]["Name"] = [[Transparency]];
+	G2L["9"]["Offset"] = Vector2.new(-1, -1);
+
+
+	-- StarterGui.ScreenGui.Notifications.Template.Buttons
+	G2L["a"] = Instance.new("ScrollingFrame", G2L["4"]);
+	G2L["a"]["Active"] = true;
+	G2L["a"]["BorderSizePixel"] = 0;
+	G2L["a"]["CanvasSize"] = UDim2.new(2, 0, 0, 0);
+	G2L["a"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+	G2L["a"]["Name"] = [[Buttons]];
+	G2L["a"]["Size"] = UDim2.new(0, 310, 0, 37);
+	G2L["a"]["ScrollBarImageColor3"] = Color3.fromRGB(0, 0, 0);
+	G2L["a"]["Position"] = UDim2.new(0, 20, 0, 74);
+	G2L["a"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+	G2L["a"]["ScrollBarThickness"] = 0;
+	G2L["a"]["BackgroundTransparency"] = 1;
+
+
+	-- StarterGui.ScreenGui.Notifications.Template.Buttons.Template
+	G2L["b"] = Instance.new("TextButton", G2L["a"]);
+	G2L["b"]["BorderSizePixel"] = 0;
+	G2L["b"]["AutoButtonColor"] = false;
+	G2L["b"]["TextSize"] = 14;
+	G2L["b"]["TextColor3"] = Color3.fromRGB(0, 0, 0);
+	G2L["b"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+	G2L["b"]["FontFace"] = Font.new([[rbxassetid://12187365364]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+	G2L["b"]["Size"] = UDim2.new(0, 74, 0, 37);
+	G2L["b"]["BackgroundTransparency"] = 0.925;
+	G2L["b"]["Name"] = [[Template]];
+	G2L["b"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+	G2L["b"]["Text"] = [[]];
+	G2L["b"]["Position"] = UDim2.new(0, 0, -1.69048, 0);
+
+
+	-- StarterGui.ScreenGui.Notifications.Template.Buttons.Template.UICorner
+	G2L["c"] = Instance.new("UICorner", G2L["b"]);
+	G2L["c"]["CornerRadius"] = UDim.new(0.1, 0);
+
+
+	-- StarterGui.ScreenGui.Notifications.Template.Buttons.Template.Label
+	G2L["d"] = Instance.new("TextLabel", G2L["b"]);
+	G2L["d"]["TextWrapped"] = true;
+	G2L["d"]["BorderSizePixel"] = 0;
+	G2L["d"]["TextScaled"] = true;
+	G2L["d"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+	G2L["d"]["TextSize"] = 14;
+	G2L["d"]["FontFace"] = Font.new([[rbxassetid://12187365364]], Enum.FontWeight.Medium, Enum.FontStyle.Normal);
+	G2L["d"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
+	G2L["d"]["BackgroundTransparency"] = 1;
+	G2L["d"]["AnchorPoint"] = Vector2.new(0.5, 0.5);
+	G2L["d"]["Size"] = UDim2.new(0.8, 0, 0.475, 0);
+	G2L["d"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+	G2L["d"]["Text"] = [[Button]];
+	G2L["d"]["Name"] = [[Label]];
+	G2L["d"]["Position"] = UDim2.new(0.5, 0, 0.5, 0);
+
+
+	-- StarterGui.ScreenGui.Notifications.Template.Buttons.UIListLayout
+	G2L["e"] = Instance.new("UIListLayout", G2L["a"]);
+	G2L["e"]["Padding"] = UDim.new(0, 6);
+	G2L["e"]["VerticalAlignment"] = Enum.VerticalAlignment.Center;
+	G2L["e"]["SortOrder"] = Enum.SortOrder.LayoutOrder;
+	G2L["e"]["FillDirection"] = Enum.FillDirection.Horizontal;
+
+
+	-- StarterGui.ScreenGui.Notifications.Template.UIStroke
+	G2L["f"] = Instance.new("UIStroke", G2L["4"]);
+	G2L["f"]["Transparency"] = 0.75;
+	G2L["f"]["Thickness"] = 1.25;
+	G2L["f"]["Color"] = Color3.fromRGB(255, 255, 255);
+
+
+	-- StarterGui.ScreenGui.Notifications.Template.Icon
+	G2L["10"] = Instance.new("ImageLabel", G2L["4"]);
+	G2L["10"]["BorderSizePixel"] = 0;
+	G2L["10"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+	G2L["10"]["Image"] = [[http://www.roblox.com/asset/?id=6034308946]];
+	G2L["10"]["Size"] = UDim2.new(0, 25, 0, 25);
+	G2L["10"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+	G2L["10"]["BackgroundTransparency"] = 1;
+	G2L["10"]["Name"] = [[Icon]];
+	G2L["10"]["Position"] = UDim2.new(0, 20, 0, 14);
 end
 
 -- Pre
-local TweenService = game:GetService("TweenService")
-local Notifications = Velora.Notifications
-local Template = Notifications.Template
+local TweenService = game:GetService('TweenService')
+Notifications = Velora.Notifications
+Template = Notifications.Template
 
 Template.Visible = false
-Template:FindFirstChild("Transparency").Enabled = true
 Template.UIGradient.Enabled = false
 Template.UIStroke.Thickness = 0
 Template.Title.TextTransparency = 1
@@ -52,32 +196,44 @@ Template.UIGradient.Offset = Vector2.new(.75,.75)
 Template.Buttons.Template.BackgroundTransparency = 1
 Template.Buttons.Template.Label.TextTransparency = 1
 Template.Buttons.Template.Visible = false
+Template:FindFirstChild('Transparency').Enabled = true
 
--- Notifier
-function Tween(a, b, c, d)
-	TweenService:Create(a, b, c):Play()
-	if d then task.wait(d) end
+if JustCreated then
+	local Manage = {}
+	
+	for Descendant in pairs(Template:GetDescendants()) do
+		if pcall(function()return Descendant.Size or error('') end) then
+			Manage[Descendant] = Descendant.Size
+		end
+	end
+	
+	workspace.Camera:GetPropertyChangedSignal('ViewportSize'):Connect(function()
+		for _Instance, Size in pairs(Manage) do
+			_Instance.Size = VeloraFunctions:GetUDim2(Size, 'Auto')
+		end
+	end)
 end
 
-function Notify(title, description, icon, duration, buttons, color)
+-- Notifier
+function Notify(data)
 	-- Variables
 	local Notification = Template:Clone()
-	local Transparency = Notification:FindFirstChild("Transparency")
-	
-	local Title, Description = tostring(title), tostring(description)
-	local Icon = icon and type(icon) == 'number' and "http://www.roblox.com/asset/?id=" .. tostring(icon)
-	local Duration = duration ~= nil and type(duration) == 'number' and duration or 5
-	local Buttons = type(buttons) == 'table' and #buttons > 0
-	local ButtonsTable = Buttons and buttons
-	local Color = color ~= nil and type(color) == 'userdata' and color.R and color.G and color.B and color
-	
+	local Transparency = Notification:FindFirstChild('Transparency')
+
+	local Title, Description = tostring(data.Title), tostring(data.Description)
+	local Icon = data.Icon and type(data.Icon) == 'number' and 'http://www.roblox.com/asset/?id=' .. tostring(data.Icon)
+	local Duration = data.Duration ~= nil and type(data.Duration) == 'number' and data.Duration or 5
+	local Buttons = type(data.Buttons) == 'table' and #data.Buttons > 0
+	local ButtonsTable = Buttons and data.Buttons
+	local Color = data.Color ~= nil and type(data.Color) == 'userdata' and data.Color.R and data.Color.G and data.Color.B and data.Color
+
 	Notification.Parent = Notifications
-	Notification.Name = "Notification"
-	Notification.Size -= UDim2.new(0, 0, 0, Notification.Buttons.AbsoluteSize.Y)
-	Notification.Title.Text = tostring(title)
-	Notification.Text.Text = tostring(description)
+	Notification.Name = 'Notification'
+	Notification.Size -= UDim2.new(0, 0, Notification.Buttons.Size.Y.Scale, Notification.Buttons.Size.Y.Offset)
+	Notification.Title.Text = Title
+	Notification.Text.Text = Description
 	Notification.Visible = true
-	
+
 	if Icon then
 		Notification.Icon.Image = Icon
 	end
@@ -85,57 +241,57 @@ function Notify(title, description, icon, duration, buttons, color)
 	if Color then
 		Notification.UIGradient.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(25, 25, 25)), ColorSequenceKeypoint.new(1.00, Color)}
 	end
-	
+
 	if not Buttons then
 		Notification.Buttons:Destroy()
 	end
-	
+
 	-- Animations
-	Tween(Transparency, TweenInfo.new(0.75), {Offset = Vector2.new(1,1)}, .75)
+	VeloraFunctions:Tween(nil, {Transparency, TweenInfo.new(0.75), {Offset = Vector2.new(1,1)}}, true)
 
 	Transparency.Enabled = false
 	Notification.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 	Notification.UIGradient.Enabled = true
-	
-	Tween(Notification.UIStroke, TweenInfo.new(.35), {Thickness = 1.25})
-	Tween(Notification.UIGradient, TweenInfo.new(.75), {Offset = Vector2.new(.5, .5)}, 1.05)
-	Tween(Notification.Title, TweenInfo.new(1), {TextTransparency = 0})
-	
+
+	VeloraFunctions:Tween(nil, {Notification.UIStroke, TweenInfo.new(.35), {Thickness = 1.25}})
+	VeloraFunctions:Tween(nil, {Notification.UIGradient, TweenInfo.new(.75), {Offset = Vector2.new(.5, .5)}}, nil, 1.05)
+	VeloraFunctions:Tween(nil, {Notification.Title, TweenInfo.new(1), {TextTransparency = 0}})
+
 	if Icon then
-		Tween(Notification.Icon, TweenInfo.new(1), {ImageTransparency = 0}, .125)
+		VeloraFunctions:Tween(nil, {Notification.Icon, TweenInfo.new(1), {ImageTransparency = 0}}, nil, .125)
 	else
 		Notification.Title.Position = Notification.Icon.Position
 		Notification.Icon:Destroy()
 		task.wait(.125)
 	end
-	
-	Tween(Notification.Text, TweenInfo.new(1), {TextTransparency = 0})
-	
+
+	VeloraFunctions:Tween(nil, {Notification.Text, TweenInfo.new(1), {TextTransparency = 0}})
+
 	if Buttons then
 		task.wait(2)
-		
-		Tween(Notification, TweenInfo.new(.5), {Size = Notification.Size + UDim2.new(0, 0, 0, Notification.Buttons.AbsoluteSize.Y)}, 1)
-		
+
+		VeloraFunctions:Tween(nil, {Notification, TweenInfo.new(.5), {Size = Notification.Size + UDim2.new(0, 0, Notification.Buttons.Size.Y.Scale, Notification.Buttons.Size.Y.Offset)}}, nil, 1)
+
 		local TemplateButton = Notification.Buttons.Template
 		local ExistingButtons = 0
 		local Clicked = false
-		
+
 		for _, button in ButtonsTable do
 			local Button = TemplateButton:Clone()
-			local Text = button.Text or "No text"
-			local Callback = button.Callback or function() warn"No callback" end
+			local Text = button.Text or 'No text'
+			local Callback = button.Callback or function() warn'No callback' end
 
 			Button.Parent = TemplateButton.Parent
 
 			Button.MouseEnter:Connect(function()
 				if not Clicked then
-					Tween(Button, TweenInfo.new(.1), {BackgroundTransparency = 0.95})
+					VeloraFunctions:Tween(nil, {Button, TweenInfo.new(.1), {BackgroundTransparency = 0.95}})
 				end
 			end)
 
 			Button.MouseLeave:Connect(function()
 				if not Clicked then
-					Tween(Button, TweenInfo.new(.1), {BackgroundTransparency = 0.925})
+					VeloraFunctions:Tween(nil, {Button, TweenInfo.new(.1), {BackgroundTransparency = 0.925}})
 				end
 			end)
 
@@ -148,10 +304,10 @@ function Notify(title, description, icon, duration, buttons, color)
 			Button.Visible = true
 			Button.Label.Text = button.Text
 
-			Tween(Button, TweenInfo.new(.4), {BackgroundTransparency = .925})
-			Tween(Button.Label, TweenInfo.new(.4), {TextTransparency = 0})
-			
-			if ExistingButtons ~= #buttons then
+			VeloraFunctions:Tween(nil, {Button, TweenInfo.new(.4), {BackgroundTransparency = .925}})
+			VeloraFunctions:Tween(nil, {Button.Label, TweenInfo.new(.4), {TextTransparency = 0}})
+
+			if ExistingButtons ~= #Buttons then
 				task.wait(.125)
 			end
 		end
@@ -159,43 +315,43 @@ function Notify(title, description, icon, duration, buttons, color)
 	else
 		task.wait(1 + Duration)
 	end
-	
+
 	if Buttons then
 		Notification.Buttons.Template:Destroy()
 		for i, Button in Notification.Buttons:GetChildren() do
-			if Button:IsA("TextButton") then
-				Tween(Button, TweenInfo.new(.4), {BackgroundTransparency = 1})
-				Tween(Button.Label, TweenInfo.new(.4), {TextTransparency = 1}, 0.09375)
+			if Button:IsA('TextButton') then
+				VeloraFunctions:Tween(nil, {Button, TweenInfo.new(.4), {BackgroundTransparency = 1}})
+				VeloraFunctions:Tween(nil, {Button.Label, TweenInfo.new(.4), {TextTransparency = 1}}, nil, .09375)
 			end
 		end
 	end
-	
+
 	task.wait(.15625)
-	
+
 	if Icon then
-		Tween(Notification.Icon, TweenInfo.new(.75), {ImageTransparency = 1})
+		VeloraFunctions:Tween(nil, {Notification.Icon, TweenInfo.new(.75), {ImageTransparency = 1}})
 	end
-	
-	Tween(Notification.Text, TweenInfo.new(.75), {TextTransparency = 1}, .09375)
-	Tween(Notification.Title, TweenInfo.new(.75), {TextTransparency = 1}, .09375)
-	Tween(Notification.UIStroke, TweenInfo.new(.2625), {Thickness = 0})
-	Tween(Notification.UIGradient, TweenInfo.new(.5625), {Offset = Vector2.new(.75, .75)}, .7875)
-	
+
+	VeloraFunctions:Tween(nil, {Notification.Text, TweenInfo.new(.75), {TextTransparency = 1}}, nil, .09375)
+	VeloraFunctions:Tween(nil, {Notification.Title, TweenInfo.new(.75), {TextTransparency = 1}}, nil, .09375)
+	VeloraFunctions:Tween(nil, {Notification.UIStroke, TweenInfo.new(.2625), {Thickness = 0}})
+	VeloraFunctions:Tween(nil, {Notification.UIGradient, TweenInfo.new(.5625), {Offset = Vector2.new(.75, .75)}}, .7875)
+
 	Notification.UIGradient.Enabled = false
 	Notification.BackgroundColor3 = Color3.fromRGB(25, 25, 25)
 	Transparency.Enabled = true
 	Transparency.Offset = Vector2.new(-1, -1)
 	Transparency.Rotation = 180
-	
-	Tween(Transparency, TweenInfo.new(0.75), {Offset = Vector2.new(1, 1)}, .75)
-	Tween(Notification, TweenInfo.new(.5), {Size = UDim2.new(0,0,0,0)}, .5)
+
+	VeloraFunctions:Tween(nil, {Transparency, TweenInfo.new(0.75), {Offset = Vector2.new(1, 1)}}, true)
+	VeloraFunctions:Tween(nil, {Notification, TweenInfo.new(.5), {Size = UDim2.new(0,0,0,0)}}, true)
 	Notification:Destroy()
 end
 
-return(function(thread, title, description, icon, duration, buttons, color)
-	if thread then
-		task.spawn(Notify, title, description, icon, duration, buttons, color)
+return(function(data, noThread)
+	if noThread then
+		Notify(data)
 	else
-		Notify(title, description, icon, duration, buttons, color)
+		task.spawn(Notify, data)
 	end
 end)
